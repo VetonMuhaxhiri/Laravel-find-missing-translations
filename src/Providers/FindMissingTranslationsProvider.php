09d2148d56b3335c887ff1c4972a0017b65f6731
Gentrit Abazi <gentritabazi01@gmail.com>
2020-05-30 16:06:25 +0200

    Fresh package

---
+++ src/Providers/FindMissingTranslationsProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace VetonMuhaxhiri\Laravelfindmissingtranslations\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FindMissingTranslationsProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
